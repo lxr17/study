9d77f47fe95d200dbeb4fbe34cdd3a5de70f7e5d
何金华 <hejh@irongbei.com>
2020-01-16 20:47:30 +0800

    完成一些题目

--- net/lanxingren/study/lesson4.py
+++ net/lanxingren/study/lesson4.py
@@ -5,11 +5,13 @@
 #     step += 1
 #     print(x)
 
+
 # sum1 = 0
 # for x in range(11):
 #     if x % 2 == 0:
 #         sum1 += x
 # print(sum1)
+
 
 # import random
 #
@@ -31,9 +33,11 @@
 #
 # print("你总共猜了%d次" % count)
 
+
 # for i in range(1, 10):
 #     for j in range(1, 10):
 #         print("%d * %d = %d" % (i, j, i * j))
+
 
 # import math
 #
@@ -51,17 +55,34 @@
 # if isSushu:
 #     print("%d是一个素数。" % a)
 
+
 # 计算器直接用python3来计算
 
-# 计算最大公约数和最小公倍数
-a = int(input())
-b = int(input())
 
-x1 = a
-while True:
-    if x1 % a == 0 and x1 % b == 0:
-        print("最大公约数为%d" % x1)
-        print("最大公倍数为%d" % (a * b / x1))
-        break
-    else:
-        x1 -= 1
+# 计算最大公约数和最小公倍数
+# a = int(input())
+# b = int(input())
+#
+# x1 = a
+# while True:
+#     if a % x1 == 0 and b % x1 == 0:
+#         print("最大公约数为%d" % x1)
+#         print("最大公倍数为%d" % (a * b / x1))
+#         break
+#     else:
+#         x1 -= 1
+
+
+# 打印三角形
+# for i in range(1, 8):
+#     for j in range(i):
+#         print("*", end="")
+#     print("")
+
+# for i in range(1, 8):
+#     for j in range(7):
+#         if i >= 7 - j:
+#             print("*", end="")
+#         else:
+#             print(" ", end="")
+#     print("")
